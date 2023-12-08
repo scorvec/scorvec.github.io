e3d90da713f4ac1db91d9c7a6ba14b699bc44ff4
scorvec <shawn@Shawn-XPS15>
2023-12-08 00:57:35 +0000

    HRRR run YYYYMMDDHH

--- hrrr_1km_ref/generate_1km_ref.sh
+++ hrrr_1km_ref/generate_1km_ref.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-rm ${PWD}/Northeast/*.png ; ${PWD}/Southeast/*.png ; ${PWD}/Northwest/*.png ; ${PWD}/Southwest/*.png
+rm ${PWD}/Northeast/*.png ;rm ${PWD}/Southeast/*.png ;rm ${PWD}/Northwest/*.png ;rm ${PWD}/Southwest/*.png
 python hrrr_ref_1km.py
 node gen_html_new.js
-sed 's|/home/shawn/public_html/hrrr_1km_ref||' anim.html > index.html
+sed 's|/home/shawn/public_html/hrrr_1km_ref/||g' anim.html > index.html
 git add -A
 git commit -m 'HRRR run YYYYMMDDHH'
 git push
